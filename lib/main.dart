dc626d79c0706e2a2ac0b73e72004e2f585b4ecd
kim130727 <kim130727@gmail.com>
2023-07-09 23:43:14 +0900

    notion api complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
-import 'dart:convert' show utf8;
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-void main() async {
+Future<List<Model>> main() async {
   const String baseUrl = "https://notion-api.splitbee.io/v1/table";
   const String today = "96006eaeb0d640b385b2693dc8f77ac8";
 
+  List<Model> instances = [];
   final url = Uri.parse('$baseUrl/$today');
   final response = await http.get(url);
   if (response.statusCode == 200) {
-    print(utf8.decode(response.bodyBytes));
-    return;
+    final List<dynamic> cells = jsonDecode(utf8.decode(response.bodyBytes));
+    print(cells[1]['출처']);
+    print(cells[1]['출처'].runtimeType);
+    for (var cell in cells) {
+      instances.add(Model.fromJson(cell));
+      print(cell['출처']);
+      print(cell['지문']);
+    }
+    print(instances);
+    return instances;
   }
   throw Error();
+}
+
+/* class Model {
+  final String cell1;
+  Model({
+    required this.cell1,
+  });
+} */
+
+class Model {
+  final String cell1;
+  Model.fromJson(Map<String, dynamic> json) : cell1 = json['출처'];
 }
